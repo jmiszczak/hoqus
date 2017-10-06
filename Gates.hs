7e6b09114a2e4b5189058698cfcbb981530cd163
Jarosław Miszczak <miszczak@iitis.pl>
2017-10-06 10:19:30 +0200

    New gates with doscription

--- Gates.hs
+++ Gates.hs
@@ -1,6 +1,22 @@
+-- | Module providing definitions of basic quantum gates, including parametrized
+-- rotation and controlled operations.
 module Gates where
 
-import Data.Complex
+import Numeric.LinearAlgebra.Data
+import Numeric.LinearAlgebra
 
-sx = [[0, 1], [1, 0]] :: [[Complex Double]]
-sz = [[1, 0], [0, -1]] :: [[Complex Double]]
+-- | Constant for pure imaginary unit
+ii = 0:+1
+
+-- | Pauli gate X, logical negation
+sx = (2><2) [0, 1, 1, 0] :: Matrix C
+
+-- | Pauli gate Y
+-- sy = -ii ( sx <> sz )
+sy = (2><2) [0, -ii, ii, 0] :: Matrix C
+
+-- | Pauli gate Z
+sz = (2><2) [1, 0, 0, -1] :: Matrix C
+
+-- | Hadamard gate
+h = 1/sqrt(2) * (2><2) [1, 1, 1, -1] :: Matrix C
