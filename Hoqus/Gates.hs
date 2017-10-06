bf7bec3f91b32319a35cb32fd0b6e589e41fc042
Jarosław Miszczak <miszczak@iitis.pl>
2017-10-06 12:03:01 +0200

    Explicit type signatures

--- Hoqus/Gates.hs
+++ Hoqus/Gates.hs
@@ -3,20 +3,24 @@
 module Hoqus.Gates where
 
 import Numeric.LinearAlgebra.Data
-import Numeric.LinearAlgebra
 
 -- | Constant for pure imaginary unit
+ii :: C
 ii = 0:+1
 
 -- | Pauli gate X, logical negation
+sx :: Matrix C
 sx = (2><2) [0, 1, 1, 0] :: Matrix C
 
 -- | Pauli gate Y
 -- sy = -ii ( sx <> sz )
+sy :: Matrix C
 sy = (2><2) [0, -ii, ii, 0] :: Matrix C
 
 -- | Pauli gate Z
+sz :: Matrix C
 sz = (2><2) [1, 0, 0, -1] :: Matrix C
 
 -- | Hadamard gate
+h :: Matrix C
 h = 1/sqrt(2) * (2><2) [1, 1, 1, -1] :: Matrix C
