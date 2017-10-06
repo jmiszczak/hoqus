78fd18605588bfa9c399f325cd39b06915b34230
Jarosław Miszczak <miszczak@iitis.pl>
2017-10-06 10:18:40 +0200

    Added main module

---
+++ Hoqus.hs
@@ -0,0 +1,9 @@
+-- | Main module for loading all parts of Hoqus package and dependiences from
+-- 'hmatrix' package.
+module Hoqus where
+
+import Numeric.LinearAlgebra.Data
+import Numeric.LinearAlgebra
+
+import Dirac
+import Gates
